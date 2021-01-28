331f2805d635a0f5c0483f9d1bdab9b892bc744d
Vicky Thakor <vicky.thakor@javaquery.com>
2021-01-28 12:20:55 +0000

    fix: code format

--- src/main/java/com/javaquery/util/Objects.java
+++ src/main/java/com/javaquery/util/Objects.java
@@ -6,27 +6,27 @@ package com.javaquery.util;
  */
 public class Objects {
 
-	/**
-	 * Returns {@code true} if the provided reference is {@code null} otherwise
-	 * returns {@code false}.
-	 *
-	 * @param obj a reference to be checked against {@code null}
-	 * @return {@code true} if the provided reference is {@code null} otherwise
-	 * {@code false}
-	 */
-	public static boolean isNull(Object obj){
-		return obj == null;
-	}
+    /**
+     * Returns {@code true} if the provided reference is {@code null} otherwise
+     * returns {@code false}.
+     *
+     * @param obj a reference to be checked against {@code null}
+     * @return {@code true} if the provided reference is {@code null} otherwise
+     * {@code false}
+     */
+    public static boolean isNull(Object obj) {
+        return obj == null;
+    }
 
-	/**
-	 * Returns {@code true} if the provided reference is non-{@code null}
-	 * otherwise returns {@code false}.
-	 *
-	 * @param obj a reference to be checked against {@code null}
-	 * @return {@code true} if the provided reference is non-{@code null}
-	 * otherwise {@code false}
-	 */
-	public static boolean nonNull(Object obj){
-		return obj != null;
-	}
+    /**
+     * Returns {@code true} if the provided reference is non-{@code null}
+     * otherwise returns {@code false}.
+     *
+     * @param obj a reference to be checked against {@code null}
+     * @return {@code true} if the provided reference is non-{@code null}
+     * otherwise {@code false}
+     */
+    public static boolean nonNull(Object obj) {
+        return obj != null;
+    }
 }
